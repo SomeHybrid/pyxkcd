c1d24276cb23f4a2c2fd324d210a3508428791b2
SomeHybrid <91648368+SomeHybrid@users.noreply.github.com>
2022-10-08 17:06:52 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# xkcd.py
+
+A simple, easy-to-use xkcd wrapper
+
+-----
+
+## Documentation
+
+Coming soon™
+
+## Quickstart
+
+### Sync mode:
+
+```py
+import xkcd
+
+client = xkcd.Client()
+
+latest = client.latest()
+print(latest.json())
+```
+
+### Async mode:
+
+```py
+import asyncio
+import xkcd
+
+client = xkcd.AsyncClient()
+
+async def main():
+    tasks = (client.random() for _ in range(25))
+    return await asyncio.gather(*tasks)
+
+asyncio.run(main())```
---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+import httpx
+import xkcd
+
+
+class Tests(unittest.TestCase):
+    def test_latest(self):
+        latest_xkcd = client.latest()
+        latest = httpx.get("https://xkcd.com/info.0.json").json()
+        self.assertEqual(latest_xkcd.num, latest["num"])
+
+    def test_get(self):
+        latest_xkcd = client.latest()
+        latest = client.get(latest_xkcd.num)
+        self.assertEqual(latest_xkcd.num, latest.num)
+
+    def test_random(self):
+        random_xkcd = client.random()
+        xkcd = client.get(random_xkcd.num)
+        self.assertEqual(random_xkcd.num, xkcd.num)
+
+    def test_response(self):
+        latest_xkcd = client.latest()
+        other_xkcd = client.random()
+        self.assertNotEqual(latest_xkcd, other_xkcd)
+
+        xkcd1 = client.get(1)  # hardcoded test case
+        resp = xkcd.Response(xkcd1.raw())
+        self.assertEqual(xkcd1, resp)
+        
+if __name__ == "__main__":
+    client = xkcd.Client()
+    unittest.main()
